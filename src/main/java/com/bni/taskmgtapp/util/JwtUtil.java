bfe3b8a8e737957e4ba8af5b460957fd0cbe0def
ariiprs <arieprasetyo987@gmail.com>
2025-05-23 14:41:22 +0700

    Your clear commit message here

--- src/main/java/com/bni/taskmgtapp/util/JwtUtil.java
+++ src/main/java/com/bni/taskmgtapp/util/JwtUtil.java
@@ -1,4 +1,36 @@
 package com.bni.taskmgtapp.util;
 
-public class jwtUtil {
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Base64;
+
+@Service
+public class JwtUtil {
+
+        private static final String SECRET = "faweljf089824hhfawefh0fawfewafawfawfawfawfawfawfawfawfawfawfawfawfawfawfaw";
+        private static final Key SIGNING_KEY = new SecretKeySpec(
+                Base64.getDecoder().decode(SECRET),
+                SignatureAlgorithm.HS256.getJcaName());
+
+        public String createToken(String username) {
+                        return Jwts.builder()
+                        .setSubject(username)
+                        .signWith(SIGNING_KEY)
+                        .compact();
+        }
+
+        public String validateToken(String token) {
+                return Jwts.parserBuilder()
+                        .setSigningKey(SIGNING_KEY)
+                        .build()
+                        .parseClaimsJws(token)
+                        .getBody()
+                        .getSubject();
+        }
+
 }
